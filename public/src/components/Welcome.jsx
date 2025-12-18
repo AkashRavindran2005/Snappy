ce27ba81fd9e9a3a21aa89df34a18ea82dc22532
R Akash <93132839+AkashRavindran2005@users.noreply.github.com>
2025-12-18 12:11:09 +0530

    Initial commit

---
+++ public/src/components/Welcome.jsx
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import Robot from "../assets/robot.gif";
+
+export default function Welcome() {
+  const [userName, setUserName] = useState("");
+
+  useEffect(() => {
+    try {
+      const userString = localStorage.getItem(
+        process.env.REACT_APP_LOCALHOST_KEY
+      );
+      
+      if (userString) {
+        const user = JSON.parse(userString);
+        setUserName(user?.username || "Friend");
+      } else {
+        setUserName("Friend");
+      }
+    } catch (error) {
+      console.error("Error parsing user data:", error);
+      setUserName("Friend");
+    }
+  }, []);
+
+  return (
+    <Container>
+      <img src={Robot} alt="Robot" />
+      <h1>
+        Welcome, <span>{userName}!</span>
+      </h1>
+      <h3>Please select a chat to Start messaging.</h3>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  flex-direction: column;
+  img {
+    height: 20rem;
+  }
+  span {
+    color: #4e0eff;
+  }
+`;
